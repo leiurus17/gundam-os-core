9e5255226a51b7465d82037cdec6b1bd646b68c8
Marlon Monzon <martianworm17@gmail.com>
2019-08-12 15:37:21 +0800

    Initial commit

---
+++ gundam-os-core/src/gundam-os-core.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : gundam-os-core.c
+ Author      : Marlon Monzon
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
